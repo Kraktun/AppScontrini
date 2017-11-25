730af8da72ce39578642e0e2d3eeca9dd48d3b14
Kraktun <30228269+Kraktun@users.noreply.github.com>
2017-11-25 21:05:19 +0100

    Add download dataset
    (Only 1 photo for testing purposes)

---
+++ download_dataset.sh
@@ -0,0 +1,8 @@
+mkdir 'GDrive'
+mkdir 'GDrive/Ingegneria del software'
+mkdir 'GDrive/Ingegneria del software/DataSet'
+mkdir 'GDrive/Ingegneria del software/DataSet/photos'
+cd 'GDrive/Ingegneria del software/DataSet/photos'
+wget --no-check-certificate 'https://docs.google.com/uc?id=11bZwcT1EkxaVor0Ux8Houl9JFD8Tpkoc&export=download' -O '0.jpg'
+ls
+
